34740aea122dcef08ad8dfdbced2c9df709b0300
Kilian-2010 <killi.zocker@gmail.com>
2020-05-22 10:15:31 +0200

    Basic HP System

---
+++ Assets/HP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+    public int Health;
+
+    void takeDMG(int DMG)
+    {
+        Health -= DMG;
+        if(Health <= 0)
+        {
+            this.gameObject.SendMessage("Dead");
+        }
+    }
+}
